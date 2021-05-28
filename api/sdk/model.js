14f6b821b99b00be0b55961139330a74eb5ca845
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-05-28 19:00:36 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/maulana4444/jst_forumm11/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
