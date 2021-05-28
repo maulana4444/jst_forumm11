dd7d51438d45c416371360249b60d259b68768fb
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-05-28 16:38:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@ bot.onText(/\/start/, (msg) => {
         click /menu to main menu`
     );   
 });
-
 bot.onText(/\/menu/, (msg) => { 
     console.log(msg)
     bot.sendMessage(
@@ -28,17 +27,42 @@ bot.onText(/\/menu/, (msg) => {
     );   
 });
 
+state = 0;
 bot.onText(/\/predict/, (msg) => { 
-    console.log(msg)
     bot.sendMessage(
         msg.chat.id,
         `Input Value i|v example 9|9`
     );   
 });
 
+state = 1;
+
 bot.on ('message',(msg) => {
-    console.log(msg);
+    if(state ==1) (
+        console.log(msg.text);
+        s = msg.text.split(" | ");
+    i = s[0]
+    v = s[1]
+    model.predict(
+        [
+            parseFloat(s[0]), // string to float
+            parseFloat(s[1])
+        ]
+    ).then((jres)=>{
+        bot.sendMessage(
+        msg.chat.id,
+        `Prediction Value v is s{jres[0]} volt`
+    );   
+        bot.sendMessage(
+        msg.chat.id,
+        `Prediction Value v is s{jres[1]} watt`
+    );   
+        res.json(jres);
+    })
+    )else(
+        state == 0)
 })
+
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
