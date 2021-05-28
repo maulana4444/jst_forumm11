9772ef5a05e95b33aead30a463713d37059d6782
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-05-28 19:18:32 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ bot.onText(/\/predict/, (msg) => {
 
 bot.on('message', (msg) => {
 if(state ==1){
-    console.log(msg.text);
+    console.log(msg.Text);
     s = msg.text.split ("|");
     i = s[0]
     v = s[1]
