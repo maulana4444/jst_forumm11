58a205f99dc972f16e9593cb3aa7432e043e0d56
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-05-28 19:50:39 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -34,47 +34,31 @@ bot.onText(/\/predict/, (msg) => {
 
 
 bot.on('message', (msg) => {
-if(state ==1){
-    console.log(msg.Text);
-    s = msg.text.split ("|");
-    i = s[0]
-    v = s[1]
-    model.predict(
-        [
-            parseFloat(s[0]),
-            parseFloat(s[1])
-        ]
-    ).then((jres)=>{
-        bot.sendMessage(
-            msg.chat.id,
-            'Predict value v is ${jres[0]} volt'
+    if(state ==1){
+        console.log(msg.Text);
+        s = msg.text.split ("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]),
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                'Predict value v is ${jres[0]} volt'
             );
-        bot.sendMessage(
-            msg.chat.id,
-            'Predict value p is ${jres[1]} watt'
-                ); 
-    })
-    state = 0 
-}else{
-    state=0
-}
+            bot.sendMessage(
+                msg.chat.id,
+                'Predict value p is ${jres[1]} watt'
+            ); 
+        })
+        state = 0 
+    }else{
+        state=0
+    }
 })
-
-// routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
-        ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
-});
-
-module.exports = r;
-
-
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
